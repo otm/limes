9e81d8e59fb94463ac957a232b7d726c88ba097e
Alexander Torstling <alexander@torstling.com>
2016-03-01 13:58:08 +0100

    example usage

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -eu
+# Example Usage: docker-start.sh --mfa=123456
 docker rm ims-daemon
 docker run --net="host" --cap-add=NET_ADMIN -it --name "ims-daemon" -v $HOME/.aws:/mnt/ims ims /bin/bash -c "ifconfig lo:0 169.254.169.254 netmask 255.255.255.255 && ./ims start --config=/mnt/ims/ims.conf $*"
