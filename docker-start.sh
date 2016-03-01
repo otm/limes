61ab53206fdea22b27b2767dbfe448ccb9b0ff6f
Alexander Torstling <alexander@torstling.com>
2016-03-01 13:44:12 +0100

    use host net

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 docker rm ims-daemon
-docker run --cap-add=NET_ADMIN -it --name "ims-daemon" -v $HOME/.aws:/mnt/ims ims /bin/bash -c "ifconfig eth0:0 169.254.169.254 netmask 255.255.255.255 && ./ims start --config=/mnt/ims/ims.conf $*"
+docker run --net="host" --cap-add=NET_ADMIN -it --name "ims-daemon" -v $HOME/.aws:/mnt/ims ims /bin/bash -c "ifconfig lo:0 169.254.169.254 netmask 255.255.255.255 && ./ims start --config=/mnt/ims/ims.conf $*"
