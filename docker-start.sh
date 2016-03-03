e99518eb4bd1bd23ebe23e2587f5954685d95e9a
Alexander Torstling <alexander@torstling.com>
2016-03-03 08:50:17 +0100

    move docker scripts into subdir

--- docker-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-# Launches the limes daemon inside a docker container. Mounts ~/.aws as a conf dir, so put your limes.conf at ~/.aws/limes.conf.
-# Example Usage: docker-start.sh --mfa=123456
-docker rm limes-daemon || true
-docker run --net="host" --cap-add=NET_ADMIN -it --name "limes-daemon" -v $HOME/.aws:/mnt/limes-conf limes /bin/bash -c "ifconfig lo:0 169.254.169.254 netmask 255.255.255.255 && ./limes start --config=/mnt/limes-conf/limes.conf $*"
-
