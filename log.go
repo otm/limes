b7227f5f65fb57d966a38dccd00b2c7794aa8d5b
Nils Lagerkvist <nils.lagerkvist@gmail.com>
2016-03-19 09:28:51 +0100

    Removed null logger and added a writer to normal logger

--- log.go
+++ log.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -14,7 +15,6 @@ const (
 	Info
 	Warning
 	Error
-	Fatal
 )
 
 // Logger is the interface used for logging
@@ -29,55 +29,46 @@ type Logger interface {
 	Level(level LogLevel)
 }
 
-// NullLogger implements
-type NullLogger struct{}
+// ConsoleLogger implements
+type ConsoleLogger struct {
+	W io.Writer
+}
 
-// Info in the null logger does nothing
-func (nl NullLogger) Info(format string, a ...interface{}) {}
+// SetOutput sets the output to w
+func (nl *ConsoleLogger) SetOutput(w io.Writer) {
+	nl.W = w
+}
 
-// Warning in the null logger does nothing
-func (nl NullLogger) Warning(format string, a ...interface{}) {}
-
-// Error in the null logger does nothing
-func (nl NullLogger) Error(format string, a ...interface{}) {}
-
-// Fatalf in the null logger does nothing
-func (nl NullLogger) Fatalf(format string, a ...interface{}) {
-	os.Exit(1)
+func (nl ConsoleLogger) output() io.Writer {
+	if nl.W == nil {
+		return os.Stderr
+	}
+	return nl.W
 }
 
 // Debug in the null logger does nothing
-func (nl NullLogger) Debug(format string, a ...interface{}) {}
-
-// Level in the null logger does nothing
-func (nl NullLogger) Level(level LogLevel) {}
-
-// ConsoleLogger implements
-type ConsoleLogger struct{}
-
-// Debug in the null logger does nothing
 func (nl ConsoleLogger) Debug(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(nl.output(), format, a...)
 }
 
 // Info in the null logger does nothing
 func (nl ConsoleLogger) Info(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(nl.output(), format, a...)
 }
 
 // Warning in the null logger does nothing
 func (nl ConsoleLogger) Warning(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(nl.output(), format, a...)
 }
 
 // Error in the null logger does nothing
 func (nl ConsoleLogger) Error(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(nl.output(), format, a...)
 }
 
 // Fatalf in the null logger does nothing
 func (nl ConsoleLogger) Fatalf(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprintf(nl.output(), format, a...)
 	os.Exit(1)
 }
 
